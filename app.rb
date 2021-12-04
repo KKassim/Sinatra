1fe7d66b9c4bf5569a374ba39d5cd4820e19ce7e
KKassim <k_master@hotmail.co,uk>
2021-12-04 20:09:34 +0000

    LoardError?

--- app.rb
+++ app.rb
@@ -1,3 +1,19 @@
-gets ruby '/secret' do
-    "Hello World"
+require 'sinatra'
+require 'sinatra/reloader'
+# require "rubygems"
+# require "bundler"
+# Bundler.require
+
+get '/' do
+  "hello world!"
 end
+
+get '/secret' do
+    "This is a secret page. Shhhhh."
+end
+
+get '/cat' do
+    "https://i.imgur.com/jFaSxym.png"
+end
+
+Sinatra::Reloader
